9818d1fdd76a7911c08ae1dc12fda85c1e44e6aa
JRejas <jose.rejas@gmail.com>
2021-07-28 20:14:09 -0400

    segundo mensaje

--- src/main/java/mi/empresa/JugandoConGit/App.java
+++ src/main/java/mi/empresa/JugandoConGit/App.java
@@ -3,5 +3,6 @@ package mi.empresa.JugandoConGit;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("segundo mensaje");
   }
 }
